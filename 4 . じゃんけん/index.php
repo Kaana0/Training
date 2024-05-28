fb431d38ec5fc6e0afedb03f12668dc1adc884e2
kaana0 <k.swechocc@gmail.com>
2024-05-28 11:51:37 +0900

    じゃんけんの修正

--- 4 . じゃんけん/index.php
+++ 4 . じゃんけん/index.php
@@ -1,69 +1,52 @@
 <?php
-// じゃんけんの手を配列に代入
-$hands = ['グー', 'チョキ', 'パー'];
 
-// プレイヤーの手がPOSTされたら
+// じゃんけんの配列
+$hands = ['ぐー', 'ちょき', 'ぱー'];
+
+// handが送信されたら
 if (isset($_POST['hand'])) {
-    // プレイヤーの手を代入
-    $playerHand = $_POST['hand'];
+    // 相手の手
+    $yourHand = $_POST['hand'];
 
-    // PCの手をランダムで選択
+    // コンピューターの手
     $key = array_rand($hands);
-    $pcHand = $hands[$key];
+    $windowsHand = $hands[$key];
 
-    // 勝敗を判定
-    if ($playerHand == $pcHand) {
-        $result ='あいこ';
-    } elseif ($playerHand == 'グー' && $pcHand == 'チョキ') {
+    // 勝ち負けの判定
+    if ($yourHand == $windowsHand) {
+        $result = 'あいこ';
+    } elseif ($yourHand == 'ぐー' && $windowsHand == 'ちょき') {
         $result = '勝ち';
-    } elseif ($playerHand == 'チョキ' && $pcHand == 'パー') {
+    } elseif ($yourHand == 'ちょき' && $windowsHand == 'ぱー') {
         $result = '勝ち';
-    } elseif ($playerHand == 'パー' && $pcHand == 'グー') {
+    } elseif ($yourHand == 'ぱー' && $windowsHand == 'ぐー') {
         $result = '勝ち';
     } else {
         $result = '負け';
     }
 }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <title>じゃんけん</title>
+    <title>じゃんけんげーむ</title>
 </head>
 <body>
+    <h1>じゃんけん勝負</h1>
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') { ?>
+        <dl>
+            <dt>自分：<?php echo htmlspecialchars($yourHand, ENT_QUOTES, 'UTF-8'); ?></dt>
+            <dt>相手：<?php echo htmlspecialchars($windowsHand, ENT_QUOTES, 'UTF-8'); ?></dt>
+            <dt>結果：<?php echo htmlspecialchars($result, ENT_QUOTES, 'UTF-8'); ?></dt>
+        </dl>
+    <?php } ?>
 
-  <h1>じゃんけん勝負</h1>
-
-  <?php if($_SERVER['REQUEST_METHOD'] === 'POST'){ ?>   
-  <dl>
-    <dt>自分：<?php echo $playerHand;  ?> </dt>
-    <dt>相手：<?php echo $pcHand;  ?></dt>
-    <dt>結果：<?php echo $result;  ?></dt>
-  </dl>
-
-  <?php
-  }
-
-  ?>
-
-  <form method="post">
-      <label> グー <input type="radio" name="hand" value="グー" required ></label>
-      <label>チョキ <input type="radio" name="hand" value="チョキ" required ></label>
-      <label>パー <input type="radio" name="hand"  value="パー" required ></label>
-      <input style="display:block;" type="submit"  value="勝負">
-
-  </form>
-
-  <?php if($_SERVER['REQUEST_METHOD'] === 'POST'){ ?>   
-
-  <p><a href="rock_paper_scissors.php">初画面</a></p>
-  <?php
-  }
-
-  ?>
-
-</div>
+    <form method="post">
+        <label> ぐー <input type="radio" name="hand" value="ぐー" required></label>
+        <label> ちょき <input type="radio" name="hand" value="ちょき" required></label>
+        <label> ぱー <input type="radio" name="hand" value="ぱー" required></label>
+        <input type="submit" value="勝負">
+    </form>
 </body>
 </html>

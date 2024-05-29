cc27ba5d65a2a821060caf337311ee1df30f0e05
kaana0 <k.swechocc@gmail.com>
2024-05-29 23:47:06 +0900

    じゃんけんの修正

--- 4 . じゃんけん/index.php
+++ 4 . じゃんけん/index.php
@@ -1,5 +1,46 @@
+<?php
+session_start();
+
+$hands = ['ぐー', 'ちょき', 'ぱー'];
+
+if (!isset($_SESSION['winCnt'])) {
+    $_SESSION['winCnt'] = 0;
+    $_SESSION['show_message'] = false;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $yourHand = isset($_POST['hand']) ? $_POST['hand'] : '';
+    $_SESSION['hand'] = $yourHand;
+
+    $key = array_rand($hands);
+    $computerHand = $hands[$key];
+
+    if ($yourHand == $computerHand) {
+        $result = 'あいこ';
+        $_SESSION['winCnt'] = 0;
+    } elseif (($yourHand == 'ぐー' && $computerHand == 'ちょき') ||
+              ($yourHand == 'ちょき' && $computerHand == 'ぱー') ||
+              ($yourHand == 'ぱー' && $computerHand == 'ぐー')) {
+                $result = '勝ち';
+                $_SESSION['winCnt']++;
+                if ($_SESSION['winCnt'] >= 2) {
+                    if (rand(1, 3) == 1) {
+                        $_SESSION['show_message'] = true;
+                    }
+                    $_SESSION['winCnt'] = 0;
+                }
+              } else {
+                $result = '負け';
+                $_SESSION['winCnt'] = 0;
+              }
+} else {
+    unset($_SESSION['hand']);
+    $_SESSION['show_massaga'] = false;
+}
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

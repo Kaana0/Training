f032d2149af7050d0584a1bf039e6b8f03b1a19b
kaana0 <k.swechocc@gmail.com>
2024-05-30 22:43:31 +0900

    じゃんけんの修正

--- 4 . じゃんけん/index.php
+++ 4 . じゃんけん/index.php
@@ -5,37 +5,7 @@ $hands = ['ぐー', 'ちょき', 'ぱー'];
 
 if (!isset($_SESSION['winCnt'])) {
     $_SESSION['winCnt'] = 0;
-    $_SESSION['show_message'] = false;
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $yourHand = isset($_POST['hand']) ? $_POST['hand'] : '';
-    $_SESSION['hand'] = $yourHand;
-
-    $key = array_rand($hands);
-    $computerHand = $hands[$key];
-
-    if ($yourHand == $computerHand) {
-        $result = 'あいこ';
-        $_SESSION['winCnt'] = 0;
-    } elseif (($yourHand == 'ぐー' && $computerHand == 'ちょき') ||
-              ($yourHand == 'ちょき' && $computerHand == 'ぱー') ||
-              ($yourHand == 'ぱー' && $computerHand == 'ぐー')) {
-                $result = '勝ち';
-                $_SESSION['winCnt']++;
-                if ($_SESSION['winCnt'] >= 2) {
-                    if (rand(1, 3) == 1) {
-                        $_SESSION['show_message'] = true;
-                    }
-                    $_SESSION['winCnt'] = 0;
-                }
-              } else {
-                $result = '負け';
-                $_SESSION['winCnt'] = 0;
-              }
-} else {
-    unset($_SESSION['hand']);
-    $_SESSION['show_massaga'] = false;
+    $_SESSION['show__message'] ="";
 }
 
 ?>
@@ -49,9 +19,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
     <h1>じゃんけんゲーム</h1>
     <form method="post">
-        <input type="radio" name="hand" value="">ぐー<br>
-        <input type="radio" name="hand" value="">ちょき<br>
-        <input type="radio" name="hand" value="">ぱー<br>
+        <input type="radio" name="hand" value="ぐー" required>ぐー<br>
+        <input type="radio" name="hand" value="ちょき" required>ちょき<br>
+        <input type="radio" name="hand" value="ぱー" required>ぱー<br>
         <button type="submit">しょうぶ！！</button>
     </form>
 </body>

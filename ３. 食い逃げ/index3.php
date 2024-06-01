87c5596cdfa9e3ef4d0d4babf3b00a74c1f7fea4
kaana0 <k.swechocc@gmail.com>
2024-06-01 20:30:09 +0900

    課題3のやり直し

--- ３. 食い逃げ/index3.php
+++ ３. 食い逃げ/index3.php
@@ -1,27 +1,45 @@
 <?php
-function lunch() {
-    $money = rand(500, 1000);
-    $lunchCost = 750;
-    $removeMoney = $money - $lunchCost;
-
-    if ($removeMoney >= 0) {
-        echo "最初のお金：{$money}円\n";
-        echo "ランチ代：{$lunchCost}円\n";
-        echo "残高：{$removeMoney}円\n";
-    } else {
-        // 食い逃げ成功率0：失敗１：成功
-        $successRate = rand(0, 1);
-        if ($successRate) {
-            echo "最初のお金：{$money}円\n";
-            echo "ランチ代：{$lunchCost}円\n";
-            echo "残金：{$money}円 食い逃げ成功!\n";
+function eatAndRun () {
+    
+    $wallet = rand(500, 1000);
+    $lunchPrice = 750;
+    $balance = $wallet - $lunchPrice;
+    //echo($balance);
+    // $balanseが0よりマイナスじゃなければ食い逃げの処理を行う
+    if ($balance < 0) {
+        $success = rand(0, 1);
+        //echo($success);
+        // 食い逃げ成功
+        if ($success == 0) {
+            echo "財布の中身は" . $wallet . "円です。";
+            echo "\n";
+            echo "ランチ代は" . $lunchPrice . "でした。";
+            echo "\n";
+            echo "残金は" . $wallet. "円です。";
+            echo "\n";
+            echo "食い逃げ成功！！";
         } else {
-            $removeMoney = 0;
-            echo "最初のお金：{$money}円\n";
-            echo "ランチ代：{$lunchCost}円\n";
-            echo "残高：{$removeMoney}円 食い逃げ失敗！全額支払った！\n";
+        // 食い逃げ失敗
+            $balance = 0;
+            echo "財布の中身は" . $wallet . "円です。";
+            echo "\n";
+            echo "ランチ代は" . $lunchPrice . "でした。";
+            echo "\n";
+            echo "残金は" . $balance . "円です。";
+            echo "\n";
+            echo "食い逃げ失敗！！残高がなくなりました！";
         }
+    } else {
+    // 代金足りる場合
+        echo "財布の中身は" . $wallet . "円です。";
+        echo "\n";
+        echo "ランチ代は" . $lunchPrice . "でした。";
+        echo "\n";
+        echo "残金は" . $balance . "円です。";
+        echo "\n";
+        echo "ごちそうさまでした！！";
     }
+    //echo($wallet);
 }
-lunch();
+eatAndRun();
 ?>

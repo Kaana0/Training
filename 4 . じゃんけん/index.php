1c89e6e827dcb70c62ccec8a3b22ceede7745dc5
kaana0 <k.swechocc@gmail.com>
2024-06-06 18:28:46 +0900

    ソースコードの整理

--- 4 . じゃんけん/index.php
+++ 4 . じゃんけん/index.php
@@ -1,34 +1,27 @@
 <?php
-// せっしょんの開始
 session_start();
 
 $hands = ['ぐー', 'ちょき', 'ぱー'];
-// せっしょんの初期化
+
 if (!isset($_SESSION['winCnt'])) {
     $_SESSION['winCnt'] = 0;
     $_SESSION['show_message'] = "";
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // あなたの手
+
     $yourHand = isset($_POST['hand']) ? $_POST['hand'] : '';
-    $_SESSION['hand'] = $yourHand;
-    //var_dump($yourHand);
-    // コンピュータの手
     $key = array_rand($hands);
     $computerHand = $hands[$key];
-    // var_dump($computerHand);
-    // exit;
-    // 勝敗の判定
+
     if ($yourHand == $computerHand) {
         $result = 'あいこ';
         $_SESSION['winCnt'] = 0;
-        $_SESSION['show_message'] = "";
     } elseif (
         ($yourHand == 'ぐー' && $computerHand == 'ちょき') ||
         ($yourHand == 'ちょき' && $computerHand == 'ぱー') ||
-        ($yourHand == 'ぱー' && $computerHand == 'ぐー')) 
-    {
+        ($yourHand == 'ぱー' && $computerHand == 'ぐー')
+    ) {
         $result = '勝ち';
         $_SESSION['winCnt']++;
         if ($_SESSION['winCnt'] >= 2) {
@@ -37,8 +30,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             } else {
                 $_SESSION['show_message'] = 'ざんねん。';
             }
-            //var_dump($_SESSION['show_message']);
-            //$_SESSION['winCnt'] = 0;
         } else {
             $_SESSION['show_message'] = "";
         }
@@ -51,7 +42,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     unset($_SESSION['hand']);
     $_SESSION['show_message'] = "";
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -62,20 +52,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
     <h1>じゃんけんしょうぶ</h1>
-    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') {?>
-    
-        <p>あなた：<?php echo htmlspecialchars($yourHand, ENT_QUOTES, 'UTF-8');?></p>
-        <p>あいて：<?php echo htmlspecialchars($computerHand, ENT_QUOTES, 'UTF-8');?></p>
-        <p>けっか：<?php echo htmlspecialchars($result, ENT_QUOTES, 'UTF-8');?></p>
-    <?php if ($_SESSION['show_message']) { ?>
-        <p><?php echo htmlspecialchars($_SESSION['show_message'], ENT_QUOTES, 'UTF-8'); ?></p>
-        <?php $_SESSION['show_message'] = "" ?>
-    <?php } ?>
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') { ?>
+        <p>あなた：<?php echo htmlspecialchars($yourHand, ENT_QUOTES, 'UTF-8'); ?></p>
+        <p>あいて：<?php echo htmlspecialchars($computerHand, ENT_QUOTES, 'UTF-8'); ?></p>
+        <p>けっか：<?php echo htmlspecialchars($result, ENT_QUOTES, 'UTF-8'); ?></p>
+        <?php if ($_SESSION['show_message']) { ?>
+            <p><?php echo htmlspecialchars($_SESSION['show_message'], ENT_QUOTES, 'UTF-8'); ?></p>
+            <?php $_SESSION['show_message'] = "" ?>
+        <?php } ?>
     <?php } ?>
     <form method="post">
-        <label>ぐー<input type="radio" name="hand" value="ぐー" <?php if (isset($_SESSION['hand']) && $_SESSION['hand'] == 'ぐー') echo 'checked';?> required></label>
-        <label>ちょき<input type="radio" name="hand" value="ちょき" <?php if (isset($_SESSION['hand']) && $_SESSION['hand'] == 'ちょき') echo 'checked';?> required></label>
-        <label>ぱー<input type="radio" name="hand" value="ぱー" <?php if (isset($_SESSION['hand']) && $_SESSION['hand'] == 'ぱー') echo 'checked';?> required></label>
+        <label>ぐー<input type="radio" name="hand" value="ぐー" <?php if ($yourHand == 'ぐー') echo 'checked'; ?> required></label>
+        <label>ちょき<input type="radio" name="hand" value="ちょき" <?php if ($yourHand == 'ちょき') echo 'checked'; ?> required></label>
+        <label>ぱー<input type="radio" name="hand" value="ぱー" <?php if ($yourHand == 'ぱー') echo 'checked'; ?> required></label>
         <input type="submit" value="しょうぶ！！">
     </form>
 </body>
--- ３. 食い逃げ/index3.php
+++ ３. 食い逃げ/index3.php
@@ -1,15 +1,10 @@
 <?php
 function eatAndRun () {
-    
     $wallet = rand(500, 1000);
     $lunchPrice = 750;
     $balance = $wallet - $lunchPrice;
-    //echo($balance);
-    // $balanseが0よりマイナスじゃなければ食い逃げの処理を行う
     if ($balance < 0) {
         $success = rand(0, 1);
-        //echo($success);
-        // 食い逃げ成功
         if ($success == 0) {
             echo "財布の中身は" . $wallet . "円です。";
             echo "\n";
@@ -19,7 +14,6 @@ function eatAndRun () {
             echo "\n";
             echo "食い逃げ成功！！";
         } else {
-        // 食い逃げ失敗
             $balance = 0;
             echo "財布の中身は" . $wallet . "円です。";
             echo "\n";
@@ -30,7 +24,6 @@ function eatAndRun () {
             echo "食い逃げ失敗！！残高がなくなりました！";
         }
     } else {
-    // 代金足りる場合
         echo "財布の中身は" . $wallet . "円です。";
         echo "\n";
         echo "ランチ代は" . $lunchPrice . "でした。";
@@ -39,7 +32,6 @@ function eatAndRun () {
         echo "\n";
         echo "ごちそうさまでした！！";
     }
-    //echo($wallet);
 }
 eatAndRun();
 ?>

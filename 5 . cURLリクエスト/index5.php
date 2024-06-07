9ab8b05f54adbde40608601b889b92dead8ad52a
kaana0 <k.swechocc@gmail.com>
2024-06-07 20:55:43 +0900

    課題5. cURLリクエストの追加

--- 5 . cURLリクエスト/index5.php
+++ 5 . cURLリクエスト/index5.php
@@ -1,22 +1,17 @@
-<?php
+<?php 
+$ch = curl_init();
 
-$curl = curl_init();
+curl_setopt_array($ch, [
+    CURLOPT_URL          => "https://umayadia-apisample.azurewebsites.net/api/persons",
+    CURLOPT_RETURNTRANSFER => true
+]);
+$result = curl_exec($ch);
 
-curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://umayadia-apisample.azurewebsites.net/api/persons',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_FOLLOWLOCATION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'GET',
-));
-
-$response = curl_exec($curl);
-
-if ($response === false) {
-    $error = curl_error($curl);
-    echo "cURL Error: $error";
+if (curl_error($ch)) {
+    $error = curl_error($ch);
+    echo $error;
 } else {
-    $data = json_decode($response, true);
+    $data = json_decode($result, true);
     if ($data && isset($data['success']) && $data['success'] === true && isset($data['data'])) {
         foreach($data['data'] as $person) {
             // var_dump($person);
@@ -25,8 +20,9 @@ if ($response === false) {
             echo "ノート：{$person['note']}<br>";
             echo "年齢：{$person['age']}<br>";
             echo "登録日：{$person['registerDate']}<br>";
+            echo "<p></p>";
         }
     }
 }
-
+curl_close($ch);
 ?>

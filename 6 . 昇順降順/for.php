a6304d38a6efe8fa1ca79ce63825530c6be8f808
kaana0 <k.swechocc@gmail.com>
2024-06-13 22:53:05 +0900

    課題 . 6 FizzBuzz for文の追加

---
+++ 6 . 昇順降順/for.php
@@ -0,0 +1,73 @@
+<?php
+if (!empty($_POST['text1']) && !empty($_POST['text2']) && !empty($_POST['text3']) && !empty($_POST['text4'])) {
+    if (isset($_POST['desc'])) {
+        $text1 = $_POST['text1'];
+        $text2 = $_POST['text2'];
+        $text3 = $_POST['text3'];
+        $text4 = $_POST['text4'];
+
+        $sum = [$text1, $text2, $text3, $text4];
+
+        $count = count($sum);
+        for ($i = 0; $i < $count; $i++) {
+            for ($j = $i + 1; $j < $count; $j++) {
+                if ($sum[$i] < $sum[$j]) {
+                    $temp = $sum[$i];
+                    $sum[$i] = $sum[$j];
+                    $sum[$j] = $temp;
+                }
+            }
+        }
+    }
+    if (isset($_POST['asc'])) {
+        $text1 = $_POST['text1'];
+        $text2 = $_POST['text2'];
+        $text3 = $_POST['text3'];
+        $text4 = $_POST['text4'];
+
+        $sum = [$text1, $text2, $text3, $text4];
+
+        $count = count($sum);
+        for ($i = 0; $i < $count; $i++) {
+            for ($j = $i + 1; $j < $count; $j++) {
+                if ($sum[$i] > $sum[$j]) {
+                    $temp = $sum[$i];
+                    $sum[$i] = $sum[$j];
+                    $sum[$j] = $temp;
+                }
+            }
+        }
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>しょうじゅんこうじゅん</title>
+</head>
+
+<body>
+
+    <form method="post" action="index.php">
+        <textarea name="text1"></textarea>
+        <textarea name="text2"></textarea>
+        <textarea name="text3"></textarea>
+        <textarea name="text4"></textarea><br>
+        <input type="submit" name="desc" value="降順">
+        <input type="submit" name="asc" value="昇順">
+    </form>
+    <p>
+        <?php
+        if (!empty($sum)) {
+            foreach ($sum as $k) {
+                echo $k;
+            }
+        }
+        ?></p>
+</body>
+
+</html>

e5f3219177933e2d81fd770fd47a4c6e87d1c0a5
kaana0 <k.swechocc@gmail.com>
2024-06-17 11:47:12 +0900

    九九表for文動的処理の修正

--- 8 . for文練習問題/9×9.php
+++ 8 . for文練習問題/9×9.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +9,7 @@
         table {
             border: 1px solid;
         }
+
         td {
             border: 1px solid;
             width: 30px;
@@ -15,33 +17,32 @@
         }
     </style>
 </head>
+
 <body>
     <h3>9×9表</h3>
     <table>
-          <tr>
-                <td> </td>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>4</td>
-                <td>5</td>
-                <td>6</td>
-                <td>7</td>
-                <td>8</td>
-                <td>9</td>
-            </tr>
-            <?php
-        for ($i = 1; $i <= 9; $i++) {
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+        </tr>
+        <?php
+        for ($i = 2; $i <= 9; $i++) {
             echo '<tr>';
-            for ($l = 1; $l <= 9; $l++) {
+            echo '<td>' . $i . '</td>';
+            for ($l = 2; $l <= 9; $l++) {
                 echo '<td>' . $i * $l . '</td>';
-                if ($l == 1) {
-                    echo '<td>' . $i . '</td>';
-                }
             }
             echo '</tr>';
         }
         ?>
     </table>
 </body>
+
 </html>

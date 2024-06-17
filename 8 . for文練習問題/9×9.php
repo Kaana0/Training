a912fedfed2e4e300b0c8fd3dbf4858e3623800f
kaana0 <k.swechocc@gmail.com>
2024-06-17 11:22:33 +0900

    九九表for文動的処理の追加

--- 8 . for文練習問題/9×9.php
+++ 8 . for文練習問題/9×9.php
@@ -1,14 +1,3 @@
-<?php
-    for ($k = 1; $k <= 9; $k++) {
-        for ($z = 1; $z <= 9; $z++) {
-            if ($z != 9) {
-                echo $z * $k;
-            } else {
-                echo $z * $k . "\n";
-            }
-        }
-    }
-?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -27,9 +16,10 @@
     </style>
 </head>
 <body>
+    <h3>9×9表</h3>
     <table>
           <tr>
-                <td></td>
+                <td> </td>
                 <td>1</td>
                 <td>2</td>
                 <td>3</td>
@@ -40,114 +30,18 @@
                 <td>8</td>
                 <td>9</td>
             </tr>
-            <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>4</td>
-                <td>5</td>
-                <td>6</td>
-                <td>7</td>
-                <td>8</td>
-                <td>9</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>2</td>
-                <td>4</td>
-                <td>6</td>
-                <td>8</td>
-                <td>10</td>
-                <td>12</td>
-                <td>14</td>
-                <td>16</td>
-                <td>18</td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>3</td>
-                <td>6</td>
-                <td>9</td>
-                <td>12</td>
-                <td>15</td>
-                <td>18</td>
-                <td>21</td>
-                <td>24</td>
-                <td>27</td>
-            </tr>
-            <tr>
-                <td>4</td>
-                <td>4</td>
-                <td>8</td>
-                <td>12</td>
-                <td>16</td>
-                <td>20</td>
-                <td>24</td>
-                <td>28</td>
-                <td>32</td>
-                <td>36</td>
-            </tr>
-            <tr>
-                <td>5</td>
-                <td>5</td>
-                <td>10</td>
-                <td>15</td>
-                <td>20</td>
-                <td>25</td>
-                <td>30</td>
-                <td>35</td>
-                <td>40</td>
-                <td>45</td>
-            </tr>
-            <tr>
-                <td>6</td>
-                <td>6</td>
-                <td>12</td>
-                <td>18</td>
-                <td>24</td>
-                <td>30</td>
-                <td>36</td>
-                <td>42</td>
-                <td>48</td>
-                <td>54</td>
-            </tr>
-            <tr>
-                <td>7</td>
-                <td>7</td>
-                <td>14</td>
-                <td>21</td>
-                <td>28</td>
-                <td>35</td>
-                <td>42</td>
-                <td>49</td>
-                <td>56</td>
-                <td>63</td>
-            </tr>
-            <tr>
-                <td>8</td>
-                <td>8</td>
-                <td>16</td>
-                <td>24</td>
-                <td>32</td>
-                <td>40</td>
-                <td>48</td>
-                <td>56</td>
-                <td>64</td>
-                <td>72</td>
-            </tr>
-            <tr>
-                <td>9</td>
-                <td>9</td>
-                <td>18</td>
-                <td>27</td>
-                <td>36</td>
-                <td>45</td>
-                <td>54</td>
-                <td>63</td>
-                <td>72</td>
-                <td>81</td>
-            </tr>
+            <?php
+        for ($i = 1; $i <= 9; $i++) {
+            echo '<tr>';
+            for ($l = 1; $l <= 9; $l++) {
+                echo '<td>' . $i * $l . '</td>';
+                if ($l == 1) {
+                    echo '<td>' . $i . '</td>';
+                }
+            }
+            echo '</tr>';
+        }
+        ?>
     </table>
 </body>
 </html>

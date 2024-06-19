4496b50f0afa21bdd5e5ecb6bc56798c87be1595
kaana0 <k.swechocc@gmail.com>
2024-06-19 11:34:50 +0900

    お問い合わせふぉーむのファイル分け

--- お問い合わせフォーム/index.php
+++ お問い合わせフォーム/index.php
@@ -20,36 +20,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>お問い合わせふぉーむ</title>
-</head>
-<body>
-    <h3>おといあわせないよう</h3>
-    <form method="post" action="index.php">
-        <select name="ramen">
             <?php
             $ramens = ['とんこつらーめん', 'しょうゆらーめん', 'しおらーめん', 'みそらーめん'];
             foreach ($ramens as $r) {
                 echo "<option value='{$r}'<?php if ((isset($ramen) && $ramen == $r)) echo 'selected'; ?>$r</option>";
             }
             ?>
-        </select><br>
-        <p></p>
-        <label>
           <?php include 'checkbox.php'; ?>
-        </label>
-        <p></p>
-        <label>
         <?php include 'radio.php'; ?>    
-        </label>
-        <p></p>
-        <textarea name="textarea" rows="5" ><?php echo htmlspecialchars($textarea, ENT_QUOTES, 'UTF-8'); ?></textarea><br>
-        <p></p>
-        <input type="submit" value="そうしんする！"> 
-    </form>
     <?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
         <h3>そうしんないよう</h3>
         <p>しゅるい：<?php echo htmlspecialchars($ramen, ENT_QUOTES, 'UTF-8'); ?></p>
@@ -63,5 +41,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <p>お問い合わせ内容：<?php echo htmlspecialchars($about, ENT_QUOTES, 'UTF-8'); ?></p>
         <p>しょうさい：<?php echo htmlspecialchars($textarea, ENT_QUOTES, 'UTF-8'); ?></p>
     <?php endif; ?>
-</body>
-</html>

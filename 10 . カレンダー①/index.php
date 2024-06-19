0de518dabcd9870f9eed1e1c959baf36ac893cfc
kaana0 <k.swechocc@gmail.com>
2024-06-19 22:12:25 +0900

    課題10. カレンダー①のfor文、レイアウトの追加

--- 10 . カレンダー①/index.php
+++ 10 . カレンダー①/index.php
@@ -20,6 +20,12 @@
             text-align: center;
             border: 1px solid;
         }
+        .backRed {
+            background-color: red;
+        }
+        .backBlue {
+            background-color: blue;
+        }
     </style>
 </head>
 <body>
@@ -28,60 +34,37 @@
         <h1>2024年6月</h1>
         <table>
             <tr>
-                <th>日</th>
-                <th>月</th>
-                <th>火</th>
-                <th>水</th>
-                <th>木</th>
-                <th>金</th>
-                <th>土</th>
+                <?php
+                $week = array("日", "月", "火", "水", "木", "金", "土");
+                $startDayIndex = date('w', strtotime('20240601'));
+                for ($i = 1; $i <= 7; $i++) {
+                    if ($week[($i + $startDayIndex) % 7] == "土") {
+                        echo '<th class="backBlue">' . $week[($i + $startDayIndex) % 7] . '</th>';
+                    } elseif ($week[($i + $startDayIndex) % 7] == "日") {
+                        echo '<th class="backRed">' . $week[($i + $startDayIndex) % 7] . '</th>';
+                    } else {
+                        echo '<th>' . $week[($i + $startDayIndex) % 7] . '</th>';
+                    }
+                }
+                ?>
             </tr>
             <tr>
-                <td>26</td>
-                <td>27</td>
-                <td>28</td>
-                <td>29</td>
-                <td>30</td>
-                <td>31</td>
-                <td>1</td>
+                <?php
+                $month = cal_days_in_month(CAL_GREGORIAN, 6, 2024);
+                for ($i = 0; $i < $startDayIndex; $i++) {
+                    echo '<td></td>';
+                }
+                for ($day = 1; $day <= $month; $day++) {
+                    echo '<td>' . $day . '</td>';
+                    if (($day + $startDayIndex) % 7 == 0) {
+                        echo '</tr><tr>';
+                    }
+                }
+                echo '</tr>';
+                ?>
             </tr>
-            <tr>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-        </tr>
-        <tr>
-            <td>9</td>
-            <td>10</td>
-            <td>11</td>
-            <td>12</td>
-            <td>13</td>
-            <td>14</td>
-            <td>15</td>
-        </tr>
-        <tr>
-            <td>16</td>
-            <td>17</td>
-            <td>18</td>
-            <td>19</td>
-            <td>20</td>
-            <td>21</td>
-            <td>22</td>
-        </tr>
-        <tr>
-            <td>23</td>
-            <td>24</td>
-            <td>25</td>
-            <td>26</td>
-            <td>27</td>
-            <td>28</td>
-            <td>29</td>
-        </tr>
-    </table>
-</div>
+        </table>
+    </div>
 </body>
+
 </html>
